84d7293220d5e4b6df1866234e42ad5b2500bd8d
LuisT23 <LuisT23@DESKTOP-Q8V3JJO>
2018-04-04 20:59:05 -0600

    tarea: datos generales

---
+++ src/Numero.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Veintitres
+ */
+public class Numero {
+    public boolean esPar(int numero){
+        return numero % 2 == 0;
+    }
+}
---
+++ src/javaapplication1/DatosGenerales.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Veintitres
+ */
+public class DatosGenerales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        System.out.println("Nombre:              Luis Adalberto");
+        System.out.println("Apellidos:           Trujillo Perez");
+        System.out.println("Edad:                21 años");
+        System.out.println("Fecha de nacimiento: 28 de enero de 1997");
+        System.out.println("Carnet:              00127717");
+        System.out.println("Telefono:            75186602");
+        //Number numer = new (Number);
+
+    }
+
+}
